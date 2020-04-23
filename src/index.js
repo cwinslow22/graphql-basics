5fff9d4ea50b81f207740b3ca59a2eb5f4b92576
Charlie Winslow <cwinslow22@yahoo.com>
2020-04-23 17:40:26 -0500

    implemented arrays using custom  types

--- src/index.js
+++ src/index.js
@@ -1,13 +1,59 @@
 import { GraphQLServer } from "graphql-yoga";
 
+// Demo User Data
+const users = [
+  {
+    id: "1",
+    name: "Charlie",
+    email: "Chark@example.com",
+    age: 38
+  },
+  {
+    id: "2",
+    name: "Sarah",
+    email: "sarah@example.com"
+  },
+  {
+    id: "3",
+    name: "Mike",
+    email: "mike@example.com"
+  }
+];
+
+const posts = [
+  {
+    id: "77",
+    title: "What a title!",
+    body: "lorem if youve got em",
+    published: true
+  },
+  {
+    id: "55",
+    title: "The Daily Beatings",
+    body: "Will continue until morale improves",
+    published: true
+  },
+  {
+    id: "70",
+    title: "Ixnay ",
+    body: "ipsem in the membrane",
+    published: false
+  },
+  {
+    id: "46",
+    title: "WIP",
+    body: "Ixnay in the membrane",
+    published: false
+  }
+];
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        greeting(name: String, position: String): String!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
-        add(numbers: [Float!]!): Float!
-        grades: [Int!]!
     }
 
     type User {
@@ -28,24 +74,26 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    add(parent, args, ctx, info) {
-      if (args.numbers.length === 0) {
-        return 0;
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
       }
-      return args.numbers.reduce((acc, cur) => {
-        return acc + cur;
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
       });
     },
-    grades(parent, args, ctx, info) {
-      return [99, 88, 94];
-    },
-    greeting(parent, args, ctx, info) {
-      if (args.name) {
-        return `Hello, ${args.name}!`;
-      } else {
-        return "Hello!";
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
       }
+      return posts.filter(post => {
+        return (
+          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.body.toLowerCase().includes(args.query.toLowerCase())
+        );
+      });
     },
+
     me() {
       return {
         id: "123098",
