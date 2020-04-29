e4e268806faad2be02235408fd86b5d432c8306d
Charlie Winslow <cwinslow22@yahoo.com>
2020-04-29 15:18:40 -0500

    added ability to delete data with mutations

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { GraphQLServer } from "graphql-yoga";
 import { v4 as uuidv4 } from "uuid";
 
 // Demo User Data
-const users = [
+let users = [
   {
     id: "1",
     name: "Charlie",
@@ -21,7 +21,7 @@ const users = [
   }
 ];
 
-const posts = [
+let posts = [
   {
     id: "77",
     title: "What a title!",
@@ -52,7 +52,7 @@ const posts = [
   }
 ];
 
-const comments = [
+let comments = [
   {
     id: "11",
     text: "My first comment!",
@@ -97,6 +97,7 @@ const typeDefs = `
 
     type Mutation {
       createUser(data: CreateUserInput!): User!
+      deleteUser(id: ID!): User!
       createPost(data: CreatePostInput!): Post!
       createComment(data: CreateCommentInput!): Comment!
     }
@@ -209,6 +210,30 @@ const resolvers = {
       users.push(user);
 
       return user;
+    },
+
+    deleteUser(parent, args, ctx, info) {
+      const userIndex = users.findIndex(user => user.id === args.id);
+
+      if (userIndex === -1) {
+        throw new Error("User not found");
+      }
+
+      const deletedUsers = users.splice(userIndex, 1);
+
+      posts = posts.filter(post => {
+        const match = post.author === args.id;
+
+        if (match) {
+          comments = comments.filter(comment => comment.post !== post.id);
+        }
+
+        return !match;
+      });
+
+      comments = comments.filter(comment => comment.author !== args.id);
+
+      return deletedUsers[0];
     },
 
     createPost(parent, args, ctx, info) {
